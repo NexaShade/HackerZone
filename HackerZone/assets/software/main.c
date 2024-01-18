ad71d5759c28706ff69fca8824812d82a3ffa68e
NexaShade <156182649+NexaShade@users.noreply.github.com>
2024-01-18 04:00:29 +0100

    Hehe

---
+++ HackerZone/assets/software/main.c
@@ -0,0 +1,7 @@
+
+#include <stdio.h>
+
+int main() {
+    char str[] = "This is a string";
+    return 0;
+}
---
+++ HackerZone/assets/software/main.s
@@ -0,0 +1,47 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 14, 0	sdk_version 14, 0
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #64
+	.cfi_def_cfa_offset 64
+	stp	x29, x30, [sp, #48]             ; 16-byte Folded Spill
+	add	x29, sp, #48
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	adrp	x8, ___stack_chk_guard@GOTPAGE
+	ldr	x8, [x8, ___stack_chk_guard@GOTPAGEOFF]
+	ldr	x8, [x8]
+	stur	x8, [x29, #-8]
+	str	wzr, [sp, #12]
+	adrp	x8, l___const.main.str@PAGE
+	add	x8, x8, l___const.main.str@PAGEOFF
+	ldr	q0, [x8]
+	str	q0, [sp, #16]
+	ldrb	w8, [x8, #16]
+	strb	w8, [sp, #32]
+	ldur	x9, [x29, #-8]
+	adrp	x8, ___stack_chk_guard@GOTPAGE
+	ldr	x8, [x8, ___stack_chk_guard@GOTPAGEOFF]
+	ldr	x8, [x8]
+	subs	x8, x8, x9
+	cset	w8, eq
+	tbnz	w8, #0, LBB0_2
+	b	LBB0_1
+LBB0_1:
+	bl	___stack_chk_fail
+LBB0_2:
+	mov	w0, #0
+	ldp	x29, x30, [sp, #48]             ; 16-byte Folded Reload
+	add	sp, sp, #64
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.section	__TEXT,__cstring,cstring_literals
+l___const.main.str:                     ; @__const.main.str
+	.asciz	"This is a string"
+
+.subsections_via_symbols
